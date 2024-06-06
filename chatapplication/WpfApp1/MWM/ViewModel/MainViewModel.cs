0df08a0d756ab940bd784e622a4119004f92f5e5
Oskar Kubala <90306268+Texas02@users.noreply.github.com>
2024-06-07 00:13:14 +0200

    Add files via upload

---
+++ chatapplication/WpfApp1/MWM/ViewModel/MainViewModel.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfApp1.Core;
+using WpfApp1.MWM.Model;
+
+namespace WpfApp1.MWM.ViewModel
+{
+    internal class MainViewModel: ObservableObject
+    {
+        public ObservableCollection<MessageModel> Messages { get; set; }
+        public ObservableCollection<ContactModel> Contacts { get; set; }
+
+        /* Commands */
+
+        public RelayCommand SendCommand { get; set; }
+
+        private ContactModel _selectedContact;
+
+        public ContactModel SelectedContact
+        {
+            get { return _selectedContact; }
+            set { _selectedContact = value;
+                OnPropertyChanged();
+                    }
+
+        }
+        
+
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set 
+            { 
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            Messages = new ObservableCollection<MessageModel>();
+            Contacts = new ObservableCollection<ContactModel>();
+
+
+            SendCommand = new RelayCommand(o => 
+            {
+                Messages.Add(new MessageModel
+                {
+                    Message = Message,
+                    FirstMessage = false 
+                });
+                Message ="";
+
+            });
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Kendrick",
+                UsernameColor = "409aff",
+                ImageSource = "https://www.zsk.poznan.pl/wp-content/uploads/2019/05/m_logo.png",
+                Message = "Hello",
+                Time = DateTime.Now,
+                IsNativeOrgin = false,
+                FirstMessage = true
+            });
+
+            for (int i = 0; i < 4; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Rysiu",
+                    UsernameColor = "409aff",
+                    ImageSource = "https://www.zsk.poznan.pl/wp-content/uploads/2019/05/m_logo.png",
+                    Message = "Hello",
+                    Time = DateTime.Now,
+                    IsNativeOrgin = false,
+                });
+
+            }
+            Messages.Add(new MessageModel
+            {
+                Username = "Kumi",
+                UsernameColor = "409aff",
+                ImageSource = "https://www.zsk.poznan.pl/wp-content/uploads/2019/05/m_logo.png",
+                Message = "Hello",
+                Time = DateTime.Now,
+                IsNativeOrgin = false,
+            });
+
+            for( int i=0; i < 5 ; i++)
+            {
+                Contacts.Add(new ContactModel
+                {
+                    Username = $"Kendrick",
+                    ImageSource = "https://i.imgur.com/BDJnYrm.jpeg",
+                    Messages = Messages
+                });
+            }
+
+        }
+
+    }
+}
---
+++ chatapplication/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+
+        }
+
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void ButtonMaximize_Click(object sender, RoutedEventArgs e)
+        {
+           if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
